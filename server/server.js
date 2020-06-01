8bef3c68f5b399ff4d81f422f0fcbef03062f2d9
Muhammad Omair <omairzamir5@gmail.com>
2020-06-01 12:33:20 +0500

    First Commit

---
+++ server/server.js
@@ -0,0 +1,25 @@
+const path = require('path');
+const http = require('http');
+const express = require('express');
+const socketIO = require('socket.io');
+
+const publicPath = path.join(__dirname + "/../public");
+const port = process.env.PORT || 3000
+let app = express();
+let server = http.createServer(app);
+let io = socketIO(server);
+
+app.use(express.static(publicPath));
+
+io.on('connection', (socket) => {
+    console.log('New User Just Connected');
+
+    socket.on('disconnect', () => {
+        console.log('User was Connected');
+    });
+});
+
+server.listen(port, () => {
+    console.log(`server is Up on port ${port}`);
+});
+
